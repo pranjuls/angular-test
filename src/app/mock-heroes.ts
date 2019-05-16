598bd3481f29d5b2d87077614724be822788271c
Pranjul <pranjuls@geekyants.com>
2019-05-16 19:25:34 +0530

    test project started

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Mr. Nice'},
+	{ id: 12, name: 'Narco'},
+	{ id: 13, name: 'Bombasto'},
+	{ id: 14, name: 'Test'},
+	{ id: 15, name: 'Maenta'},
+	{ id: 16, name: 'hero no 1'},
+	{ id: 17, name: 'SOme random'},
+	{ id: 18, name: 'Unknown'},
+	{ id: 19, name: 'Dragon'},
+	{ id: 20, name: 'failure'}
+];
